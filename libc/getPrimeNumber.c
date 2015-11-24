abfd8ef3d5bb3531e9e83697b28df7ddcef99a63
gnieark <gnieark@free.fr>
2015-11-24 22:25:45 +0100

    c

---
+++ libc/getPrimeNumber.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char *argv[])
+{
+    if ( argc != 2 ){
+        printf( "usage: ./getPrimeNumber [integer]\n");
+        return 0;
+    }
+    else{
+        
+        int limit= atoi(argv[1]);
+        int *primes, i, isPrime,count=2, number=3;
+        primes=(int*)malloc(2*sizeof(int));
+        primes[0]=2;
+        primes[1]=3;
+
+        while( count <= limit){
+            isPrime=1;
+            for (i=0; ((primes[i] <= sqrt(number)) && (i < count)) ; i=i+1){
+             if(number%primes[i] == 0){
+                 isPrime=0;
+                 break;
+             }
+            }
+            if(isPrime){
+                count++;
+               primes=realloc(primes,count*sizeof(int));
+                primes[count -1]=number;
+            }
+            number+=2;
+        }
+        printf("%d\n",primes[count -1]);
+    }
+    return 0;
+}
