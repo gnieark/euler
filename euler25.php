f2e07f9a03626dadd8dea0428aadeeb88871c501
Gnieark <gnieark@free.fr>
2015-11-25 20:35:23 +0100

    25

--- euler25.php
+++ euler25.php
@@ -1,26 +1,25 @@
 <?php
 /*
-* Gnieark's anwser to euler problem 25
-* https://projecteuler.net/problem=25
+* Gnieark's anwser to euler problem 26
+* https://projecteuler.net/problem=26
 */
-
-//see  https://fr.wikipedia.org/wiki/Suite_de_Fibonacci#Avec_la_formule_de_Binet
-echo intval((log(10)*999 + (log(5)/2) ) / log(1.61803398875)) +1;
-
-/*
-Very long solution that didn't test :
-
-$i=1;
-$j=2;
-$count=2;
-while(strlen($i)<1000){
-    $new=gmp_add($i,$j);
-    $i=$j;
-    $j=$new;
+$beastDenominateur=0;
+$longestCycle=0;
+for ($denominateur=2; $denominateur < 1000; $denominateur ++){
+  
+  $reste=1;
+  $restes=array();
+  $count=0;
+  while (!in_array($reste,$restes)){
+    $restes[]=$reste;
+    $reste=$reste*10;
+    $reste=fmod($reste,$denominateur);
     $count++;
+  }  
+  if($count > $longestCycle){
+    $longestCycle=$count;
+    $beastDenominateur=$denominateur;
+  }
 }
 
-
-echo $count;
-
-*/
+echo $beastDenominateur."\n";
