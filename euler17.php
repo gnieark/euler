6e26650c6b36ac50d751aa3816939a78a881af7e
Gnieark <gnieark@free.fr>
2018-12-31 18:05:30 +0100

    fix #1

--- euler17.php
+++ euler17.php
@@ -42,7 +42,7 @@ function convert_number_to_words($number) {
         19                  => 'nineteen',
         20                  => 'twenty',
         30                  => 'thirty',
-        40                  => 'fourty',
+        40                  => 'forty',
         50                  => 'fifty',
         60                  => 'sixty',
         70                  => 'seventy',
